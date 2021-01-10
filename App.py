9b8dd5cd80dee29160b712fcff1dac5faf9957d6
aycanocal <aycanocal28@gmail.com>
2021-01-10 04:44:56 +0300

    Flash messages

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
@@ -17,14 +17,14 @@ class Meeting(db.Model):
     topic = db.Column(db.String(100))
     date = db.Column(db.Date)
     startHour = db.Column(db.Time)
-    finishHour = db.Column(db.Time)
-    participants = db.Column(db.String(100))
+    endHour = db.Column(db.Time)
+    participants = db.Column(db.String(1000))
 
-    def __init__(self, topic, date, startHour, finishHour, participants):
+    def __init__(self, topic, date, start_hour, end_hour, participants):
         self.topic = topic
         self.date = date
-        self.startHour = startHour
-        self.finishHour = finishHour
+        self.startHour = start_hour
+        self.endHour = end_hour
         self.participants = participants
 
 
@@ -39,17 +39,20 @@ def add():
     if request.method == 'POST':
         topic = request.form['topic']
         date = request.form['date']
-        startHour = request.form['startHour']
-        finishHour = request.form['finishHour']
+        start_hour = request.form['startHour']
+        end_hour = request.form['endHour']
         participants = request.form['participants']
 
-        meeting = Meeting(topic, date, startHour, finishHour, participants)
+        meeting = Meeting(topic, date, start_hour, end_hour, participants)
         db.session.add(meeting)
         db.session.commit()
 
+        flash('Meeting successfully added')
+
         return redirect(url_for('Index'))
 
-@app.route('/update', methods=['GET','POST'])
+
+@app.route('/update', methods=['GET', 'POST'])
 def update():
     if request.method == 'POST':
         meeting = Meeting.query.get(request.form.get('id'))
@@ -57,25 +60,26 @@ def update():
         meeting.topic = request.form['topic']
         meeting.date = request.form['date']
         meeting.startHour = request.form['startHour']
-        meeting.finishHour = request.form['finishHour']
+        meeting.endHour = request.form['endHour']
         meeting.participants = request.form['participants']
 
         db.session.add(meeting)
         db.session.commit()
 
+        flash('Meeting successfully updated')
+
         return redirect(url_for('Index'))
 
-@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
 def delete(id):
     meeting = Meeting.query.get(id)
     db.session.delete(meeting)
     db.session.commit()
 
+    flash('Meeting successfully deleted')
+
     return redirect(url_for('Index'))
-
-
-
-
 
 
 if __name__ == "__main__":
